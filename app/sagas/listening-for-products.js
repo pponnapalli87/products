5043daf05a90e50d0567711029ce2322daf29f71
Prasanth Ponnapalli <pponnapalli.87@gmail.com>
2019-08-12 14:00:33 +1000

    Extract fetch into a separate method for ease of testing

--- app/sagas/listening-for-products.js
+++ app/sagas/listening-for-products.js
@@ -1,17 +1,19 @@
-import { put, take } from 'redux-saga/effects'
+import { call, put, take } from 'redux-saga/effects'
 import { camelizeKeys } from 'humps'
 
 import { GET_PRODUCTS, GET_PRODUCTS_SUCCEEDED, GET_PRODUCTS_FAILED } from '../actions/products'
 
+function fetchApi(url) {
+  return fetch(url)
+    .then(response => response.json())
+}
+
 export default function* listeningForProducts() {
   try {
-    while (true) {
-      yield take(GET_PRODUCTS)
-      const result = yield fetch('https://api.jsonbin.io/b/5cae9a54fb42337645ebcad3')
-                      .then(response => response.json())
+    yield take(GET_PRODUCTS)
 
-      yield put({ type: GET_PRODUCTS_SUCCEEDED, payload: { products: camelizeKeys(result) } })
-    }
+    const result = yield call(fetchApi, 'https://api.jsonbin.io/b/5cae9a54fb42337645ebcad3')
+    yield put({ type: GET_PRODUCTS_SUCCEEDED, payload: { products: camelizeKeys(result) } })
   }
   catch (error) {
     yield put({ type: GET_PRODUCTS_FAILED, payload: { error } })
